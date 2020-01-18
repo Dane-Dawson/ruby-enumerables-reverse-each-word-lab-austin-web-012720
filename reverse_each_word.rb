e6f0f4e47d6a6b7522c0565ad3c65372334d727f
Dane P Dawson <dane.p.dawson@gmail.com>
2020-01-18 05:00:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,6 @@
 def reverse_each_word(sentence)
   one_word = sentence.split(/ /)
   reversed_words = one_word.each { |word| word.reverse }
-  
+  end_string = reversed_words.join(" ")
+  end_string
 end
